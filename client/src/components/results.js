80ce3b2d0e86fb88c047436f3437069680b08219
Sang Bae <sw2.bae@gmail.com>
2020-08-14 17:14:19 -0500

    rev06_mapBooks Added

--- client/src/components/results.js
+++ client/src/components/results.js
@@ -1,22 +1,31 @@
 import React from "react";
 // import { Link, useLocation } from "react-router-dom";
+import { useUserContext } from "../utils/BookContext";
 
 function Result() {
+    const books = useUserContext();
+    console.log(books);
+    // const { id, authors, description, image, link, title } = useUserContext();
     return (
         <div className="container card mt-3">
             <p className="mt-3 mb-3 text-left">Results</p>
-            <div className="container card mt-3 mb-3">
-                <div className="container">
-                    <p className="mt-3 mb-3 text-left">Title</p>
-                    <button>View</button>
-                    <button>Save</button>
-                </div>
-                <p className="mt-3 mb-3 text-left">Written By Author</p><hr />
-                <div className="row">
-                    <img className="col-3"></img>
-                    <p className="col-9">description</p>
-                </div>
-            </div>
+
+            {books.map((book, index) => {
+                return (
+                    <div className="container card mt-3 mb-3" key={index}>
+                        <div className="container">
+                            <p className="mt-3 mb-3 text-left">{book.title}</p>
+                            <button>View</button>
+                            <button>Save</button>
+                        </div>
+                        <p className="mt-3 mb-3 text-left">Written By {book.authors}</p><hr />
+                        <div className="row">
+                            <img className="col-3" src={book.image} alt={index} />
+                            <p className="col-9">{book.description}</p>
+                        </div>
+                    </div>
+                )
+            })}
         </div >
     );
 }
